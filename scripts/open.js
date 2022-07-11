df7725e673600dc5f879f5a76caa612b2e674bee
Vladislav Ponomarev <kitch@MacBook-Air-Vladislav.local>
2022-07-11 16:56:05 +0500

    wheel animation

--- scripts/open.js
+++ scripts/open.js
@@ -1,11 +1,18 @@
 const closedCar = document.querySelector('.closed')
 const openCar = document.querySelector('.open')
 const print = document.querySelector('.print')
+const wheel1 = document.querySelector('.first')
+const wheel2 = document.querySelector('.second')
+const wheel3 = document.querySelector('.third')
+
 
 closedCar.addEventListener('click', ()=>{
     if(openCar.classList.contains('open')){
         openCar.classList.add('active');
         closedCar.classList.remove('active');
+        wheel1.classList.add('inactive');
+        wheel2.classList.add('inactive');
+        wheel3.classList.add('inactive');
         print.classList.remove('active');
     }
 })
@@ -16,4 +23,3 @@ openCar.addEventListener('click', ()=>{
         print.classList.add('active');
     }
 })
-
