1596b64ab63d4fbb742a9caa0c9e274a75fad357
Vladislav Ponomarev <kitch@MacBook-Air-Vladislav.local>
2022-07-13 16:31:58 +0500

    add wheel animation

--- scripts/open.js
+++ scripts/open.js
@@ -1,25 +1,21 @@
 const closedCar = document.querySelector('.closed')
 const openCar = document.querySelector('.open')
 const print = document.querySelector('.print')
-const wheel1 = document.querySelector('.first')
-const wheel2 = document.querySelector('.second')
-const wheel3 = document.querySelector('.third')
-
+const close = document.querySelector('.close')
 
 closedCar.addEventListener('click', ()=>{
     if(openCar.classList.contains('open')){
         openCar.classList.add('active');
+        close.classList.add('active');
         closedCar.classList.remove('active');
-        wheel1.classList.add('inactive');
-        wheel2.classList.add('inactive');
-        wheel3.classList.add('inactive');
         print.classList.remove('active');
     }
 })
-openCar.addEventListener('click', ()=>{
+close.addEventListener('click', ()=>{
     if(closedCar.classList.contains('closed')){
         closedCar.classList.add('active');
         openCar.classList.remove('active');
+        close.classList.remove('active');
         print.classList.add('active');
     }
 })
